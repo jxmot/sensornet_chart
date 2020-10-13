badfb2584460e2c346ca0978f6b2de5bcccb1cb6
jxmot <webdev@stupiddog.net>
2020-10-13 16:28:50 -0500

    improved error detection

--- assets/js/histchart.js
+++ assets/js/histchart.js
@@ -109,99 +109,108 @@ $(document).on('config', function(e, _config) {
 // incoming history data...
 $(document).on('hist_show', function(e, _hist) {
     var hist = JSON.parse(_hist);
-    consolelog('hist_show - records = '+hist.data.length);
-    // if data is a SINGLE sensor then....
-    if(hist.query.dev_id.length === 1) {
-        chart.destroy();
-        histchart_cfg.series = [];
-        histchart_cfg.title.text = names[hist.query.dev_id[0]];
-        loadSeries(hist.data);
-        histchart_cfg.series = [
-            {name: '°F', data: temps},
-            {name: '%RH', data: humid}
-        ];
-        histchart_cfg.colors = [
-            colors[hist.query.dev_id[0]][0],
-            colors[hist.query.dev_id[0]][1]
-        ],
-        histchart_cfg.yaxis = [
-            {
-                min: mins.t,
-                title: {
-                    text: 'Temp °F',
-                    style: {
-                        color: colors[hist.query.dev_id[0]][0]
-                    }
-                },
-                labels: {
-                    style: {
-                        colors: [colors[hist.query.dev_id[0]][0]]
-                    },
-                    formatter: function (val) {
-                        return val.toFixed(0)
-                    }
-                }
-            },
-            {
-                min: mins.h,
-                opposite: true,
-                title: {
-                    text: '%RH',
-                    style: {
-                        color: colors[hist.query.dev_id[0]][1]
-                    }
-                },
-                labels: {
-                    style: {
-                        colors: [colors[hist.query.dev_id[0]][1]]
-                    },
-                    formatter: function (val) {
-                        return val.toFixed(0)
-                    }
-                }
-            }
-        ];
+    if(hist.data === null) {
+        alert('no data found');
+        consolelog('hist_show - data is null - '+_hist);
+        if(hist.err !== undefined) {
+            alert('ERROR - err = '+JSON.stringify(hist.err));
+            consolelog('hist_show - err = '+_hist);
+        }
     } else {
-        chart.destroy();
-        histchart_cfg.series = [];
-        histchart_cfg.title.text = '';
-        hist.query.dev_id.forEach(function(devid, index) {
-            histchart_cfg.title.text += names[devid];
-            if(index < (hist.query.dev_id.length - 1))
-                histchart_cfg.title.text += ', ';
-        });
-
-        loadTempSeries(hist.data);
-        // blank series object
-        var tmp = {
-            name: '',
-            data: []
-        }
-        histchart_cfg.colors = [];
-        // create one series for each sensor and put it
-        // into the chart config(no references!)
-        for(ix = 0;ix < hist.query.dev_id.length; ix++) {
-            tmp.name = names[hist.query.dev_id[ix]];
-            tmp.data = JSON.parse(JSON.stringify(temps[hist.query.dev_id[ix]]));
-            histchart_cfg.series.push(JSON.parse(JSON.stringify(tmp)));
-            histchart_cfg.colors.push(colors[hist.query.dev_id[ix]][0]);
-        }
-
-        histchart_cfg.yaxis = [
-            {
-                title: {
-                    text: 'Temp °F'
+        consolelog('hist_show - records = '+hist.data.length);
+        // if data is a SINGLE sensor then....
+        if(hist.query.dev_id.length === 1) {
+            chart.destroy();
+            histchart_cfg.series = [];
+            histchart_cfg.title.text = names[hist.query.dev_id[0]];
+            loadSeries(hist.data);
+            histchart_cfg.series = [
+                {name: '°F', data: temps},
+                {name: '%RH', data: humid}
+            ];
+            histchart_cfg.colors = [
+                colors[hist.query.dev_id[0]][0],
+                colors[hist.query.dev_id[0]][1]
+            ],
+            histchart_cfg.yaxis = [
+                {
+                    min: mins.t,
+                    title: {
+                        text: 'Temp °F',
+                        style: {
+                            color: colors[hist.query.dev_id[0]][0]
+                        }
+                    },
+                    labels: {
+                        style: {
+                            colors: [colors[hist.query.dev_id[0]][0]]
+                        },
+                        formatter: function (val) {
+                            return val.toFixed(0)
+                        }
+                    }
                 },
-                labels: {
-                    formatter: function (val) {
-                        return val.toFixed(0)
+                {
+                    min: mins.h,
+                    opposite: true,
+                    title: {
+                        text: '%RH',
+                        style: {
+                            color: colors[hist.query.dev_id[0]][1]
+                        }
+                    },
+                    labels: {
+                        style: {
+                            colors: [colors[hist.query.dev_id[0]][1]]
+                        },
+                        formatter: function (val) {
+                            return val.toFixed(0)
+                        }
                     }
                 }
+            ];
+        } else {
+            chart.destroy();
+            histchart_cfg.series = [];
+            histchart_cfg.title.text = '';
+            hist.query.dev_id.forEach(function(devid, index) {
+                histchart_cfg.title.text += names[devid];
+                if(index < (hist.query.dev_id.length - 1))
+                    histchart_cfg.title.text += ', ';
+            });
+    
+            loadTempSeries(hist.data);
+            // blank series object
+            var tmp = {
+                name: '',
+                data: []
             }
-        ];
+            histchart_cfg.colors = [];
+            // create one series for each sensor and put it
+            // into the chart config(no references!)
+            for(ix = 0;ix < hist.query.dev_id.length; ix++) {
+                tmp.name = names[hist.query.dev_id[ix]];
+                tmp.data = JSON.parse(JSON.stringify(temps[hist.query.dev_id[ix]]));
+                histchart_cfg.series.push(JSON.parse(JSON.stringify(tmp)));
+                histchart_cfg.colors.push(colors[hist.query.dev_id[ix]][0]);
+            }
+    
+            histchart_cfg.yaxis = [
+                {
+                    title: {
+                        text: 'Temp °F'
+                    },
+                    labels: {
+                        formatter: function (val) {
+                            return val.toFixed(0)
+                        }
+                    }
+                }
+            ];
+        }
+        // draw it!
+        newChart();
     }
-    // draw it!
-    newChart();
 });
 
 var choices = {
